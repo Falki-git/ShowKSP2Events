06837c51c74e195fdc5c7781ca4005e37ac3ffe0
Falki <72734856+Falki-git@users.noreply.github.com>
2023-04-09 20:47:36 +0200

    Delete ShowKSP2EventsProject/obj directory

--- ShowKSP2EventsProject/obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ShowKSP2Events
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "com.github.falki.showksp2events";
-        public const string PLUGIN_NAME = "ShowKSP2Events";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
